ca872676fd8d4d4b88a93013dbd9313550c77bf6
Hoang Van Tien <hvtien0827@gmail.com>
2026-06-29 22:56:19 +0700

    Số chính phương

---
+++ MarisaOJ/Problem11. Số chính phương/Square.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fixenter cin.ignore(numeric_limits<streamsize>::max(),'\n')
+using ll = long long;
+using ull = unsigned long long;
+using ldb = long double;
+
+bool square(ull a){
+    ll y=sqrt(a);
+    return y*y==a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ull a; cin >> a;
+    cout<<(square(a) ? "YES" : "NO");
+    return 0;
+}
---
+++ MarisaOJ/Problem11. Số chính phương/Số chính phương.md
@@ -0,0 +1,31 @@
+Link problem : https://marisaoj.com/problem/11
+Giới hạn thời gian: 1000 ms
+Giới hạn bộ nhớ: 256 MB
+
+Cho số nguyên a. Hãy xác định xem a có phải số chính phương không?
+
+### Input
+
+- Số nguyên a.
+
+### Output
+
+- In ra `YES` nếu a là số chính phương, ngược lại in ra `NO`.
+
+### Điều kiện
+
+- (1≤a≤$10^{18}$).
+
+### Ví dụ
+
+Input:
+
+```
+16
+```
+
+Output:
+
+```
+YES
+```
