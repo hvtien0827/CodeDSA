a342ce5316a7df2579e35ff2f506208cb621f1e6
Hoang Van Tien <hvtien0827@gmail.com>
2026-06-29 23:30:39 +0700

    Đếm số chính phương dương <= n

---
+++ MarisaOJ/Problem12. Đếm số chính phương/Đếm số chính phương.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fixenter cin.ignore(numeric_limits<streamsize>::max(),'\n')
+using ll = long long;
+using ull = unsigned long long;
+using ldb = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ull a; cin>>a;
+    //Số lượng số chính phương dương <= n = phần nguyên của căn n
+    cout<<(ll)sqrt(a);
+    return 0;
+}
